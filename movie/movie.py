782dcedd44a08cd14ae228017b5b93a0f06b94ff
Matis Byar <matis.byar.06@icloud.com>
2024-09-27 11:24:37 +0200

    movies

--- movie/movie.py
+++ movie/movie.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, jsonify, make_response
 import json
-import sys
-from werkzeug.exceptions import NotFound
+
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -9,14 +8,62 @@ PORT = 3200
 HOST = '0.0.0.0'
 
 with open('{}/databases/movies.json'.format("."), 'r') as jsf:
-   movies = json.load(jsf)["movies"]
+    movies = json.load(jsf)["movies"]
+
+
+def write(movies):
+    with open('{}/databases/movies.json'.format("."), 'w') as f:
+        json.dump(movies, f)
+
 
 # root message
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
+
+
+@app.route("/json", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(movies), 200)
+    return res
+
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie), 200)
+            return res
+    return make_response(jsonify({"error": "Movie ID not found"}), 400)
+
+
+@app.route("/movies/<movieid>", methods=['POST'])
+def add_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            return make_response(jsonify({"error": "movie ID already exists"}), 409)
+
+    movies.append(req)
+    write(movies)
+    res = make_response(jsonify({"message": "movie added"}), 200)
+    return res
+
+
+@app.route("/movies/<movieid>/<rate>", methods=['PUT'])
+def update_movie_rating(movieid, rate):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            movie["rating"] = rate
+            res = make_response(jsonify(movie), 200)
+            return res
+
+    res = make_response(jsonify({"error": "movie ID not found"}), 201)
+    return res
+
 
 if __name__ == "__main__":
-    #p = sys.argv[1]
-    print("Server running in port %s"%(PORT))
+    # p = sys.argv[1]
+    print("Server running in port %s" % (PORT))
     app.run(host=HOST, port=PORT)
