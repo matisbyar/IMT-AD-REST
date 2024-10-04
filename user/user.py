e82db645fb8a4c977bcd4fbedbef0d1af0d15208
jln <nguyenjulienpro@gmail.com>
2024-10-04 11:11:41 +0200

    showtime.py

--- user/user.py
+++ user/user.py
@@ -3,6 +3,7 @@ import requests
 import json
 from werkzeug.exceptions import NotFound
 
+
 app = Flask(__name__)
 
 PORT = 3203
@@ -14,6 +15,19 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+@app.route("/users", methods=['GET'])
+def get_json():
+   res = make_response(jsonify(users), 200)
+   return res
+
+@app.route("/user/<userId>", methods = ['GET'])
+def get_user_by_id(userId):
+   for user in users:
+      if str(user["id"]) == str(userId):
+         res = make_response(jsonify(user), 200)
+         return res
+   return make_response(jsonify({"error": "No user with this ID"}), 400)
 
 
 if __name__ == "__main__":
