c3b2ef4f973a0ce2abe463f1d8cbb74957a38252
Matis Byar <matis.byar.06@icloud.com>
2024-10-10 20:10:19 +0200

    improved users

--- user/user.py
+++ user/user.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
 import json
-from werkzeug.exceptions import NotFound
 
+import requests
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
@@ -10,26 +9,65 @@ PORT = 3203
 HOST = '0.0.0.0'
 
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
 
 @app.route("/users", methods=['GET'])
 def get_json():
-   res = make_response(jsonify(users), 200)
-   return res
+    res = make_response(jsonify(users), 200)
+    return res
 
-@app.route("/user/<userId>", methods = ['GET'])
+
+@app.route("/user/<userId>", methods=['GET'])
 def get_user_by_id(userId):
-   for user in users:
-      if str(user["id"]) == str(userId):
-         res = make_response(jsonify(user), 200)
-         return res
-   return make_response(jsonify({"error": "No user with this ID"}), 400)
+    for user in users:
+        if str(user["id"]) == str(userId):
+            res = make_response(jsonify(user), 200)
+            return res
+    return make_response(jsonify({"error": "No user with this ID"}), 400)
+
+
+@app.route("/user/<userId>/bookings/movies", methods=['GET'])
+def get_movies_from_usersbooking(userId):
+    bookings_url = f"http://{request.remote_addr}:3201/bookings/{userId}"
+    bookings = requests.get(bookings_url)
+
+    if bookings.status_code == 200:
+        bookings_list = bookings.json()
+
+        print(bookings_list)
+
+        movies = []
+        for booking in bookings_list["dates"]:
+            print(booking["movies"])
+            for movie in booking["movies"]:
+                movies.append(movie)
+
+        if len(movies) == 0:
+            return make_response(jsonify({"error": "User has no bookings"}), 409)
+
+        movies_detailed = []
+        for movie in movies:
+            print(movie)
+
+            movie_url =  f"http://{request.remote_addr}:3200/movie/{movie}"
+            movie_fetched = requests.get(movie_url)
+
+            if movie_fetched.status_code == 200:
+                movies_detailed.append(movie_fetched.json()["movie"])
+                print(movie_fetched.json()["movie"])
+            else:
+                return make_response(jsonify({"error": "An error occurred while fetching a movie"}), 409)
+
+    else:
+        return make_response(jsonify({"error": "User has no bookings"}), 409)
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
