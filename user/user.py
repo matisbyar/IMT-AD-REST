830dcf33c5360212a927c6611c5cbe15d7e68d18
Matis Byar <matis.byar.06@icloud.com>
2024-10-18 10:19:37 +0200

    improved users

--- user/user.py
+++ user/user.py
@@ -12,6 +12,12 @@ with open('{}/databases/users.json'.format("."), "r") as jsf:
     users = json.load(jsf)["users"]
 
 
+def write(bookings):
+    data = {"users": bookings}
+    with open('{}/databases/users.json'.format("."), 'w') as f:
+        json.dump(data, f)
+
+
 @app.route("/", methods=['GET'])
 def home():
     return "<h1 style='color:blue'>Welcome to the User service!</h1>"
@@ -30,6 +36,48 @@ def get_user_by_id(userId):
             res = make_response(jsonify(user), 200)
             return res
     return make_response(jsonify({"error": "No user with this ID"}), 400)
+
+
+@app.route("/users/<userId>", methods=['POST'])
+def add_user(userId):
+    req = request.get_json()
+
+    for user in users:
+        if str(user["id"]) == str(userId):
+            return make_response(jsonify({"error": "user ID already exists"}), 409)
+
+    users.append(req)
+    write(users)
+
+    return make_response(jsonify({"message": "user added"}), 200)
+
+
+@app.route("/user/<userId>", methods=['DELETE'])
+def del_user(userId):
+    for user in users:
+        if str(user["id"]) == str(userId):
+            users.remove(user)
+            write(users)
+
+            return make_response(jsonify(user), 200)
+
+    return make_response(jsonify({"error": "user not found"}), 400)
+
+
+@app.route("/user/<userId>", methods=['PUT'])
+def update_user_lastactive(userId):
+    last_active = request.args.get('last_active')
+
+    for user in users:
+        if str(user["id"]) == str(userId):
+            user["last_active"] = last_active
+            write(users)
+
+            res = make_response(jsonify(user), 200)
+            return res
+
+    res = make_response(jsonify({"error": "user not found"}), 201)
+    return res
 
 
 @app.route("/user/<userId>/bookings/movies", methods=['GET'])
@@ -58,6 +106,20 @@ def get_movies_from_usersbooking(userId):
             return make_response(jsonify({"error": "An error occurred while fetching a movie"}), 409)
 
     return make_response(jsonify({"movies": movies_detailed}), 200)
+
+
+@app.route("/help", methods=['GET'])
+def get_help_users():
+    help = [
+        {"path_and_method": "GET /users", "description": "Get all users"},
+        {"path_and_method": "GET /user/<userId>", "description": "Get user by ID"},
+        {"path_and_method": "GET /user/<userId>/bookings/movies",
+         "description": "Retrieve all user's bookings by their ID"},
+        {"path_and_method": "PUT /user/<userId>", "description": "Update user last_active timestamp by their ID"},
+        {"path_and_method": "DELETE /user/<userId>", "description": "Delete user by ID"},
+        {"path_and_method": "GET /help", "description": "Get help"}
+    ]
+    return make_response(jsonify(help), 200)
 
 
 if __name__ == "__main__":
